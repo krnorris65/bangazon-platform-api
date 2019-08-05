516f67e3be8d491d58b110d4f4cda2508d8d5856
Kristen Norris <krnorris65@gmail.com>
2019-08-05 16:06:29 -0500

    created test for all payments

---
+++ TestBangazonAPI/TestPaymentTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TestBangazonAPI
+{
+    public class TestPaymentTypes
+    {
+        [Fact]
+        public async Task Test_Get_All_PaymentTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/paymentTypes");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentTypes = JsonConvert.DeserializeObject<List<PaymentType>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(paymentTypes.Count > 0);
+            }
+        }
+    }
+}
