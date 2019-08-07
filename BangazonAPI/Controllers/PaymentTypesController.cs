0573e63008049f45f673cc4226d59b5467e2ead6
Kristen Norris <krnorris65@gmail.com>
2019-08-07 13:09:39 -0500

    crud functionality for paymenttypes with passing tests

--- BangazonAPI/Controllers/PaymentTypesController.cs
+++ BangazonAPI/Controllers/PaymentTypesController.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -73,28 +74,175 @@ namespace BangazonAPI.Controllers
         }
 
         // GET api/paymentTypes/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetPaymentType")]
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+            if (!PaymentTypeExists(id))
+            {
+                return NotFound();
+            }
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT p.Id as payId, p.AcctNumber, p.[Name], p.CustomerId, c.Id as custId, c.FirstName, c.LastName
+                                        FROM PaymentType p
+                                        JOIN Customer c ON p.CustomerId = c.Id
+                                        WHERE p.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    PaymentType paymentType = null;
+                    if(reader.Read())
+                    {
+                        Customer customer = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("custId")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                        };
+
+                        paymentType = new PaymentType
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("payId")),
+                            AcctNumber = reader.GetInt32(reader.GetOrdinal("AcctNumber")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            Customer = customer
+                        };
+                    }
+                    return Ok(paymentType);
+                }
+            }
         }
 
         // POST api/paymentTypes
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Post([FromBody]PaymentType paymentType)
         {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = @"
+                        INSERT INTO PaymentType([Name], AcctNumber, CustomerId)
+                        OUTPUT INSERTED.Id
+                        VALUES (@name, @acctNumber, @customerId)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@name", paymentType.Name));
+                    cmd.Parameters.Add(new SqlParameter("@acctNumber", paymentType.AcctNumber));
+                    cmd.Parameters.Add(new SqlParameter("@customerId", paymentType.CustomerId));
+
+                    paymentType.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetPaymentType", new { id = paymentType.Id }, paymentType);
+                }
+            }
         }
 
         // PUT api/paymentTypes/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody]PaymentType paymentType)
         {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            UPDATE PaymentType
+                            SET [Name] = @name,
+                                AcctNumber = @acctNumber,
+                                CustomerId = @customerId
+                            WHERE Id = @id
+                        ";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@name", paymentType.Name));
+                        cmd.Parameters.Add(new SqlParameter("@acctNumber", paymentType.AcctNumber));
+                        cmd.Parameters.Add(new SqlParameter("@customerId", paymentType.CustomerId));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!PaymentTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
         // DELETE api/paymentTypes/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using(SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM PaymentType
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        if(rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                if(!PaymentTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        private bool PaymentTypeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = "SELECT Id FROM PaymentType WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
         }
     }
 }
--- TestBangazonAPI/TestPaymentTypes.cs
+++ TestBangazonAPI/TestPaymentTypes.cs
@@ -5,6 +5,8 @@ using Xunit;
 using BangazonAPI.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
 namespace TestBangazonAPI
 {
@@ -15,14 +17,7 @@ namespace TestBangazonAPI
         {
             using (var client = new APIClientProvider().Client)
             {
-                /*
-                    ARRANGE
-                */
 
-
-                /*
-                    ACT
-                */
                 var response = await client.GetAsync("/api/paymentTypes");
 
 
@@ -34,6 +29,128 @@ namespace TestBangazonAPI
                 */
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 Assert.True(paymentTypes.Count > 0);
+                Assert.NotNull(paymentTypes[0].Customer);
+            }
+        }
+        [Fact]
+        public async Task Test_Get_Single_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/paymentTypes/3");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentType = JsonConvert.DeserializeObject<PaymentType>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal(3, paymentType.Id);
+                Assert.Equal(16797, paymentType.AcctNumber);
+                Assert.Equal("diners-club-enroute", paymentType.Name);
+                Assert.NotNull(paymentType);
+
+            }
+        }
+
+        [Fact]
+        public async Task Test_Create_And_Delete_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                PaymentType newPaymentType = new PaymentType
+                {
+                    Name = "Visa",
+                    AcctNumber = 12345,
+                    CustomerId = 1
+                };
+
+                var PaymentAsJSON = JsonConvert.SerializeObject(newPaymentType);
+
+                var response = await client.PostAsync(
+                    "/api/paymentTypes",
+                    new StringContent(PaymentAsJSON, Encoding.UTF8, "application/json")
+                );
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var NewVisa = JsonConvert.DeserializeObject<PaymentType>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal(newPaymentType.Name, NewVisa.Name);
+                Assert.Equal(newPaymentType.AcctNumber, NewVisa.AcctNumber);
+                Assert.Equal(newPaymentType.CustomerId, NewVisa.CustomerId);
+
+                /*
+                    ACT
+                */
+                var deleteResponse = await client.DeleteAsync($"/api/paymentTypes/{NewVisa.Id}");
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Modify_PaymentType()
+        {
+            string NewPaymentTypeName = "AmEx";
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    PUT section
+                 */
+                PaymentType ModifiedPayment = new PaymentType
+                {
+                    Name = NewPaymentTypeName,
+                    AcctNumber = 28845,
+                    CustomerId = 2
+                };
+                var ModifiedPaymentAsJSON = JsonConvert.SerializeObject(ModifiedPayment);
+
+                var response = await client.PutAsync(
+                    "/api/paymentTypes/2",
+                    new StringContent(ModifiedPaymentAsJSON, Encoding.UTF8, "application/json")
+                );
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                /*
+                    GET section
+                 */
+                var GetVisa = await client.GetAsync("/api/paymentTypes/2");
+                GetVisa.EnsureSuccessStatusCode();
+
+                string GetPaymentBody = await GetVisa.Content.ReadAsStringAsync();
+                PaymentType NewVisa = JsonConvert.DeserializeObject<PaymentType>(GetPaymentBody);
+
+                Assert.Equal(HttpStatusCode.OK, GetVisa.StatusCode);
+                Assert.Equal(NewPaymentTypeName, NewVisa.Name);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Get_NonExistant_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/paymentTypes/9999999");
+
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Delete_NonExistant_PaymentType()
+        {
+            using(var client = new APIClientProvider().Client)
+            {
+                var response = await client.DeleteAsync("/api/paymentTypes/9999999");
+                Assert.False(response.IsSuccessStatusCode);
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             }
         }
     }
